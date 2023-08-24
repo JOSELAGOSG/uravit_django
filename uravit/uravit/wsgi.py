f0f30f83eb0b3b2a220b8bedf846de3fbef7e5c4
José Lagos <joseantoniolagosg@gmail.com>
2023-08-24 14:29:49 -0300

    initial commit

---
+++ uravit/uravit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for uravit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uravit.settings')
+
+application = get_wsgi_application()
