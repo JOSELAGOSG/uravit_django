a0fc017297a097ec945dca2e9590e438ceda73be
José Lagos <joseantoniolagosg@gmail.com>
2023-10-01 23:54:19 -0300

    models PEP8

--- uravit/trial/models.py
+++ uravit/trial/models.py
@@ -11,9 +11,8 @@ class Juicio(models.Model):
     fecha_juicio_oral = models.DateTimeField()
     fiscal = models.CharField(max_length=30, null=True)
 
-
     def __str__(self):
-        return(f'{self.ruc}')
+        return f'{self.ruc}'
 
     def get_absolute_url(self):
         return reverse('trial:juicio-detail', args=[self.pk])
@@ -26,51 +25,50 @@ class Juicio(models.Model):
 
     def get_create_testigo_url(self):
         return reverse('trial:testigo-create', args=[self.pk])
-    
+
     def get_create_perito_url(self):
         return reverse('trial:perito-create', args=[self.pk])
 
 class Persona(models.Model):
     nombre = models.CharField(max_length=30)
     rut = models.CharField(max_length=30)
-    direccion = models.CharField (max_length=200, blank=True)
+    direccion = models.CharField(max_length=200, blank=True)
     correo = models.EmailField(null=True, blank=True)
-    telefono = models.CharField(max_length=50, null=True, blank=True) 
+    telefono = models.CharField(max_length=50, null=True, blank=True)
     bool_esta_notificada = models.BooleanField(default=False)
     observaciones = models.TextField(null=True, blank=True)
 
     class Meta:
         abstract = True
 
-
 class Testigo(Persona):
-    juicio = models.ForeignKey(Juicio, related_name="testigos", on_delete=models.CASCADE )
+    juicio = models.ForeignKey(Juicio, related_name="testigos", on_delete=models.CASCADE)
     edad = models.IntegerField(null=True, blank=True)
     bool_pauta_lista = models.BooleanField(default=False)
     link_pauta_necesidades = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.nombre}'
-    
+
     def get_absolute_url(self):
         return reverse('trial:testigo-detail', args=[self.pk])
 
     def get_update_url(self):
         return reverse('trial:testigo-update', args=[self.pk])
-    
+
     def get_delete_url(self):
         return reverse('trial:testigo-delete', args=[self.pk])
 
 class Victima(Persona):
-    juicio = models.ForeignKey(Juicio, related_name="victimas", on_delete=models.CASCADE )
+    juicio = models.ForeignKey(Juicio, related_name="victimas", on_delete=models.CASCADE)
 
     edad = models.IntegerField(null=True, blank=True)
     bool_pauta_lista = models.BooleanField(default=False)
     link_pauta_necesidades = models.URLField(null=True, blank=True)
-    
+
     def __str__(self):
         return f'{self.nombre}'
-    
+
     def get_absolute_url(self):
         return reverse('trial:victima-detail', args=[self.pk])
 
@@ -79,9 +77,9 @@ class Victima(Persona):
 
     def get_delete_url(self):
         return reverse('trial:victima-delete', args=[self.pk])
-    
+
 class Perito(Persona):
-    juicio = models.ForeignKey(Juicio, related_name="peritos", on_delete=models.CASCADE )
+    juicio = models.ForeignKey(Juicio, related_name="peritos", on_delete=models.CASCADE)
     institucion = models.CharField(max_length=30)
 
     def __str__(self):
@@ -92,7 +90,7 @@ class Perito(Persona):
 
     def get_update_url(self):
         return reverse('trial:perito-update', args=[self.pk])
-    
+
     def get_delete_url(self):
         return reverse('trial:perito-delete', args=[self.pk])
 
@@ -111,7 +109,7 @@ class Equipo(models.Model):
         return reverse('trial:equipo-update', args=[self.pk])
 
     def get_delete_url(self):
-        return reverse('trial:equipo-delete', args=[self.pk]) 
+        return reverse('trial:equipo-delete', args=[self.pk])
 
 class Perfil(models.Model):
     usuario = models.OneToOneField(User, related_name='perfil', on_delete=models.CASCADE)
@@ -119,7 +117,7 @@ class Perfil(models.Model):
 
     def get_update_url(self):
         return reverse('trial:perfil-update', args=[self.pk])
-    
+
     def get_delete_url(self):
         return reverse('trial:perfil-delete', args=[self.pk])
 
@@ -140,7 +138,7 @@ class Apoyo(models.Model):
     descripcion = models.TextField(null=True, blank=True)
 
     # Campos relacionados a Traslado
-    traslado_tipo = models.CharField(choices=TIPOS_TRASLADOS_CHOICES, max_length=2, null=True, blank=True) #Si es traslado local o desde otra region
+    traslado_tipo = models.CharField(choices=TIPOS_TRASLADOS_CHOICES, max_length=2, null=True, blank=True)  # Si es traslado local o desde otra región
     traslado_vehiculo = models.CharField(choices=VEHICULOS_CHOICES, max_length=2, null=True, blank=True)
 
     # Campos relacionados a Estadía
@@ -155,6 +153,6 @@ class Apoyo(models.Model):
     def clean(self):
         if self.victima and self.testigo:
             raise ValidationError("Un apoyo no puede estar relacionado tanto con una víctima como con un testigo al mismo tiempo.")
-        
+
     def get_absolute_url(self):
         return reverse('trial:apoyo-detail', args=[self.pk])
