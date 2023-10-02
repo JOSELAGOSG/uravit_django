b549ea9841c8f7d17d78b73654915d4907df995f
José Lagos <joseantoniolagosg@gmail.com>
2023-10-01 23:57:45 -0300

    views PEP8

--- uravit/trial/views.py
+++ uravit/trial/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import (
-    get_object_or_404,
-    redirect,
-)
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from .models import (
     Juicio,
@@ -12,32 +9,20 @@ from .models import (
     Apoyo,
     Equipo,
 )
-from django.views.generic.edit import (
-    CreateView,
-    DeleteView,
-    UpdateView,
-)
-from django.views.generic import (
-    ListView,
-    DetailView,
-    TemplateView,
-)
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.contrib.auth.decorators import login_required
-
 from django.utils.decorators import method_decorator
-
 from django.core.exceptions import ObjectDoesNotExist
-
 from django.views.defaults import permission_denied
 
-#Juicio Views
+# Juicio Views
 
 class JuicioCreateView(CreateView):
     template_name = 'trial/juicio/juicio_form.html'
     model = Juicio
     fields = '__all__'
-    
+
 class JuicioDetailView(DetailView):
     template_name = 'trial/juicio/juicio_detail.html'
     model = Juicio
@@ -47,7 +32,6 @@ class JuicioListView(ListView):
     template_name = 'trial/juicio/juicio_list.html'
     model = Juicio
     context_object_name = 'juicios'
-     
 
 class JuicioUpdateView(UpdateView):
     template_name = 'trial/juicio/juicio_form.html'
@@ -60,8 +44,7 @@ class JuicioDeleteView(DeleteView):
     context_object_name = 'juicio'
     success_url = reverse_lazy('trial:juicio-list')
 
-
-#TESTIGO Views
+# TESTIGO Views
 
 class TestigoDetailView(DetailView):
     template_name = 'trial/testigo/testigo_detail.html'
@@ -72,7 +55,7 @@ class TestigoCreateView(CreateView):
     template_name = 'trial/testigo/testigo_form.html'
     model = Testigo
     fields = ['nombre', 'rut', 'direccion', 'correo', 'telefono', 'bool_esta_notificada', 'observaciones', 'edad', 'bool_pauta_lista', 'link_pauta_necesidades']
-    
+
     def form_valid(self, form):
         juicio = get_object_or_404(Juicio, pk=self.kwargs['juicio_pk'])
         self.object = form.save(commit=False)
@@ -89,14 +72,13 @@ class TestigoDeleteView(DeleteView):
     template_name = 'trial/testigo/testigo_confirm_delete.html'
     model = Testigo
     context_object_name = 'testigo'
-    
+
     def get_success_url(self):
         testigo = self.object
         juicio_pk = testigo.juicio.pk
         return reverse_lazy('trial:juicio-detail', kwargs={'pk': juicio_pk})
 
-
-#VICTIMA Views
+# VICTIMA Views
 
 class VictimaDetailView(DetailView):
     template_name = 'trial/victima/victima_detail.html'
@@ -124,14 +106,13 @@ class VictimaDeleteView(DeleteView):
     template_name = 'trial/victima/victima_confirm_delete.html'
     model = Victima
     context_object_name = 'victima'
-    
+
     def get_success_url(self):
-        victima = self.object 
+        victima = self.object
         juicio_pk = victima.juicio.pk
         return reverse_lazy('trial:juicio-detail', kwargs={'pk': juicio_pk})
 
-
-#PERITO Views
+# PERITO Views
 
 class PeritoDetailView(DetailView):
     template_name = 'trial/perito/perito_detail.html'
@@ -149,7 +130,7 @@ class PeritoCreateView(CreateView):
         self.object.juicio = juicio
         self.object.save()
         return redirect(self.object.juicio.get_absolute_url())
-    
+
 class PeritoUpdateView(UpdateView):
     template_name = 'trial/perito/perito_form.html'
     model = Perito
@@ -161,7 +142,7 @@ class PeritoDeleteView(DeleteView):
     context_object_name = 'perito'
 
     def get_success_url(self):
-        perito = self.object 
+        perito = self.object
         juicio_pk = perito.juicio.pk
         return reverse_lazy('trial:juicio-detail', kwargs={'pk': juicio_pk})
 
@@ -175,7 +156,7 @@ class PerfilCreateView(CreateView):
 
 class PerfilListView(ListView):
     template_name = 'trial/perfil/perfil_list.html'
-    model = Perfil 
+    model = Perfil
     context_object_name = 'perfiles'
 
 class PerfilUpdateView(UpdateView):
@@ -191,49 +172,51 @@ class PerfilDeleteView(DeleteView):
     success_url = reverse_lazy('trial:perfil-list')
 
 # Apoyo CRUD
+
 class ApoyoVictimaCreateView(CreateView):
     template_name = 'trial/apoyo/apoyo_form.html'
     model = Apoyo
     fields = [
-        'tipo', 
+        'tipo',
         'equipo_a_cargo',
-        'estado', 
-        'descripcion', 
+        'estado',
+        'descripcion',
         'traslado_tipo',
         'traslado_vehiculo',
         'estadia_con_alimentacion',
         'asistencia_medica_tipo',
         'traductor_idioma'
-        ]
+    ]
+
     def form_valid(self, form):
         victima = get_object_or_404(Victima, pk=self.kwargs['victima_pk'])
         self.object = form.save(commit=False)
         self.object.victima = victima
         self.object.save()
-        print("Apoyo guardado exitosamente")
         return redirect(self.object.victima.get_absolute_url())
 
 class ApoyoTestigoCreateView(CreateView):
     template_name = 'trial/apoyo/apoyo_form.html'
     model = Apoyo
     fields = [
-        'tipo', 
+        'tipo',
         'equipo_a_cargo',
-        'estado', 
-        'descripcion', 
+        'estado',
+        'descripcion',
         'traslado_tipo',
         'traslado_vehiculo',
         'estadia_con_alimentacion',
         'asistencia_medica_tipo',
         'traductor_idioma'
-        ]
+    ]
+
     def form_valid(self, form):
         testigo = get_object_or_404(Testigo, pk=self.kwargs['testigo_pk'])
         self.object = form.save(commit=False)
         self.object.testigo = testigo
         self.object.save()
         return redirect(self.object.testigo.get_absolute_url())
-    
+
 class ApoyoDetailView(DetailView):
     template_name = 'trial/apoyo/apoyo_detail.html'
     model = Apoyo
@@ -246,17 +229,11 @@ class ApoyoDetailView(DetailView):
             perfil = request.user.perfil
         except Exception:
             perfil = None
-        
+
         if not request.user.is_staff and apoyo.equipo_a_cargo != perfil.equipo:
             return permission_denied(request, exception="Acceso Prohibido")
         return super().get(request, *args, **kwargs)
 
-'''
-    ApoyoListView entrega:
-        -Todos los apoyos a los usuarios staff y superusuarios
-        -Los apoyos relacionados al equipo del usuario si es que el usuario tiene perfil
-        -Nada si es que el usuario no tiene perfil
-'''
 class ApoyoListView(ListView):
     template_name = 'trial/apoyo/apoyo_list.html'
     model = Apoyo
@@ -283,16 +260,16 @@ class ApoyoUpdateView(UpdateView):
     template_name = 'trial/apoyo/apoyo_form.html'
     model = Apoyo
     fields = [
-        'tipo', 
+        'tipo',
         'equipo_a_cargo',
-        'estado', 
-        'descripcion', 
+        'estado',
+        'descripcion',
         'traslado_tipo',
         'traslado_vehiculo',
         'estadia_con_alimentacion',
         'asistencia_medica_tipo',
         'traductor_idioma'
-        ]
+    ]
 
 class ApoyoEstadoUpdateView(UpdateView):
     template_name = 'trial/apoyo/apoyo_estado_update.html'
@@ -303,9 +280,9 @@ class ApoyoVictimaDeleteView(DeleteView):
     template_name = 'trial/apoyo/apoyo_confirm_delete.html'
     model = Apoyo
     context_object_name = 'apoyo'
-    
+
     def get_success_url(self):
-        apoyo = self.object 
+        apoyo = self.object
         victima_pk = apoyo.victima.pk
         return reverse_lazy('trial:victima-detail', kwargs={'pk': victima_pk})
 
@@ -313,9 +290,9 @@ class ApoyoTestigoDeleteView(DeleteView):
     template_name = 'trial/apoyo/apoyo_confirm_delete.html'
     model = Apoyo
     context_object_name = 'apoyo'
-    
+
     def get_success_url(self):
-        apoyo = self.object 
+        apoyo = self.object
         testigo_pk = apoyo.testigo.pk
         return reverse_lazy('trial:testigo-detail', kwargs={'pk': testigo_pk})
 
@@ -338,7 +315,7 @@ class UserPerfilView(TemplateView):
             perfil = None
 
         return context
-    
+
 # Equipo CRUD
 class EquipoCreateView(CreateView):
     template_name = 'trial/equipo/equipo_form.html'
